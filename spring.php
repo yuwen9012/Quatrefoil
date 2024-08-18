01049c1b68c52182f475b7cf92a8ebb48574aeea
yuwen9012 <156352342+yuwen9012@users.noreply.github.com>
2024-08-19 02:36:44 +0800

    四季測驗結果(第一區塊完成)

---
+++ spring.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- 引入 Poppins 字體 -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap">
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <!-- FontAwesome CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/spring.css">
+    <link rel='stylesheet' type='text/css' href='./css/Navbar.css'>
+
+    <title>spring</title>
+</head>
+<body>
+     <nav>
+    <div class='brand'>
+      <a href="#"><img src="./images/icon/brand.png" width="120px" height="27px"/></a>
+    </div>
+    <div class='menu_center'>
+      <a href="test_new.php">四季色彩測驗</a>
+      <a href="#">四季商城</a>
+    </div>
+    <div class='menu_right'>
+      <a href="#"><img src="./images/icon/shirt-black.png" width="20px" height="20px"/></a>
+      <a href="#"><img src="./images/icon/user.png" width="20px" height="20px"/></a>
+    </div>
+  </nav>
+
+     <!-- 第一區塊 -->
+     <div class="container-fluid block1" style="background-color: #f8d2d6;">
+        <div class="row">
+            <div class="col-1 left-section">
+            </div>
+            <div class="col-10 center-section">
+                <div class="box1">
+                    <div class="row">
+                        <div class="col-12-section">
+                            <p class="t1">您的四季色彩類型：<span>春季型</span></p>
+                            <p class="t2">#開朗 #陽光 #朝氣感 #熱情</p>
+                            <p class="t3">笑起來春天來臨😳、充滿活力、請支援防曬、眼神靈動、親切感100、（感覺皮很薄）、四季型色素最淺、（看起來很年輕）、🌸🍃🌼</p>
+                            <div class="row">
+                                <div class="col-2 left-section">
+                                   <div class="box2">膚色</div>
+                                   <div class="box2">臉頰</div>
+                                   <div class="box2">眼睛</div>
+                                   <div class="box2">嘴唇</div>
+                                </div>
+                                <div class="col-2 left-section">
+                                   <div class="box3">皮膚基調偏暖色調，<br>膚色明亮白皙具透明感</div>
+                                   <div class="box3">偏暖橘色調</div>
+                                   <div class="box3">明亮的茶色、棕色系</div>
+                                   <div class="box3">偏橘的粉色</div>
+                                </div>
+                                <div class="col-2 right-section">
+                                   <div class="box4">代表明星</div>
+                                   <div class="box5">飾品</div>
+                                </div>
+                                <div class="col-2 right-section">
+                                   <div class="box6">石原里美、橋本環奈、<br>徐若瑄、李智恩、<br>山下智久、岡田將生、<br>千葉雄大、坂口健太郎</div>
+                                   <div class="box7">亮面金色、香檳色、<br>玫瑰金</div>
+                                </div>
+                                <div class="col-4 right-section">
+                                    <img class="Image" src="images/spring.png" alt="Description of the image" class="img-fluid">
+                                </div>
+                            </div> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-1 right-section">
+            </div>
+        </div>
+    </div>
+
+<body>
+</html>
+
---
+++ winter.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- 引入 Poppins 字體 -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap">
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <!-- FontAwesome CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/winter.css">
+    <link rel='stylesheet' type='text/css' href='./css/Navbar.css'>
+
+    <title>winter</title>
+</head>
+<body>
+     <nav>
+    <div class='brand'>
+      <a href="#"><img src="./images/icon/brand.png" width="120px" height="27px"/></a>
+    </div>
+    <div class='menu_center'>
+      <a href="test_new.php">四季色彩測驗</a>
+      <a href="#">四季商城</a>
+    </div>
+    <div class='menu_right'>
+      <a href="#"><img src="./images/icon/shirt-black.png" width="20px" height="20px"/></a>
+      <a href="#"><img src="./images/icon/user.png" width="20px" height="20px"/></a>
+    </div>
+  </nav>
+
+     <!-- 第一區塊 -->
+     <div class="container-fluid block1" style="background-color: #c3e4f2;">
+        <div class="row">
+            <div class="col-1 left-section">
+            </div>
+            <div class="col-10 center-section">
+                <div class="box1">
+                    <div class="row">
+                        <div class="col-12-section">
+                            <p class="t1">您的四季色彩類型：<span>冬季型</span></p>
+                            <p class="t2">#鮮明 #時尚感 #個性 #白皙</p>
+                            <p class="t3">冷豔時尚、氣場強大、對比美、高冷王者👑、🕶️✨❄️、（霸總的感覺）、黑白我主場、聚光燈帶了、眼神殺、（有點距離感）</p>
+                            <div class="row">
+                                <div class="col-2 left-section">
+                                   <div class="box2">膚色</div>
+                                   <div class="box2">臉頰</div>
+                                   <div class="box2">眼睛</div>
+                                   <div class="box2">嘴唇</div>
+                                </div>
+                                <div class="col-2 left-section">
+                                   <div class="box3" style="font-size: 14px;">皮膚基調偏冷色調，<br>白皙厚實、具透明感</div>
+                                   <div class="box3">霧面冷粉</div>
+                                   <div class="box3">黑棕或烏黑，瞳孔分明</div>
+                                   <div class="box3">玫瑰粉</div>
+                                </div>
+                                <div class="col-2 right-section">
+                                   <div class="box4">代表明星</div>
+                                   <div class="box5">飾品</div>
+                                </div>
+                                <div class="col-2 right-section">
+                                   <div class="box6">小松菜奈、剛力彩芽、<br>高圓圓、樸秀英、<br>菅田將暉、松本潤<br>山崎賢人、本木雅宏</div>
+                                   <div class="box7">亮面銀、白金、<br>純色寶石</div>
+                                </div>
+                                <div class="col-4 right-section">
+                                    <img class="Image" src="images/winter.png" alt="Description of the image" class="img-fluid" style="margin: 0px 30px;">
+                                </div>
+                            </div> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-1 right-section">
+            </div>
+        </div>
+    </div>
+
+<body>
+</html>
