3a6cdb67140f803e48b6159531bc7f5381878c8e
helen1020912 <74821347+helen1020912@users.noreply.github.com>
2024-08-21 16:52:59 +0800

    Add files via upload

--- front_end/navbar.php
+++ front_end/navbar.php
@@ -2,22 +2,20 @@
 <html>
 <head>
 
-<link rel='stylesheet' type='text/css' href='../CSS/Navbar.css'>
-<link rel='stylesheet' type='text/css' href='../CSS/Navbar_rwd.css'>
+<link rel='stylesheet' type='text/css' href='css/Navbar.css'>
 </head>
 
 <nav>
     <div class='nav_brand'>
-      <a href="home.php"><img src="../images/icon/brand.png"/></a>
+      <a href="home.php"><img src="images/icon/brand.png"/></a>
     </div>
     <div class='nav_center'>
-      <a href="#"><span>四季色彩分析</span></a>
+      <a href="season_type_test.php"><span>四季色彩分析</span></a>
       <a href="#"><span>四季商城</span></a>
-      <a href="#"><span>四季論壇</span></a>
     </div>
     <div class='nav_right'>
-      <a href="#"><img src="../images/icon/shirt-black.png" width="24px" height="24px"/></a>
-      <a href="account_setting.php"><img src="../images/icon/user.png" width="23px" height="23px"/></a>
+      <a href="#"><img src="images/icon/shirt-black.png" width="24px" height="24px"/></a>
+      <a href="account_setting.php"><img src="images/icon/user.png" width="23px" height="23px"/></a>
       <div id="ham_menu"></div>
     </div>
   </nav>
