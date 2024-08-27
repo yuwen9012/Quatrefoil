83a2f776ad66a6dffa9a2d1ee748a2bba5f7da06
yuwen9012 <156352342+yuwen9012@users.noreply.github.com>
2024-08-28 00:22:52 +0800

    RWD

--- season_type_test.php
+++ season_type_test.php
@@ -29,92 +29,116 @@
   ?>
 
      <!-- 第一區塊 -->
-  <div class="container-fluid block1">
+  <div class="container-fluid block" style="background-color: #409FAC;">
     <br><br><br><br><br><br>
     <div class="row">
-        <div class="col-md-2 left-section">
+        <div class="col-md-2 col-sm-1 col-1 left-section">
         </div>
-        <div class="col-md-4 center-section">
+        <div class="col-md-4 col-sm-10 col-10 center-section">
             <p class="t1">-基本條件設定-</p>
-            <div class="box1-2">
-                <p class="p1-1">男</p>
-                <p class="p1-2">女</p>
-                <label for="radio">
-                        <input type="radio" class="btn1" name="gender" value=1 required="required">
-                </label>
-                <label for="radio">
-                        <input type="radio" class="btn2" name="gender" value=0>
-                </label>
-            </div>
-            <br>
-            <div class="box1-2">
-                <p class="p1-3">自然瞳色</p>
-                <p class="p1-4">非自然瞳色</p>
-                <label for="radio">
-                        <input type="radio" class="btn3" name="lenses" value=1 required="required">
-                </label>
-                <label for="radio">
-                        <input type="radio" class="btn4" name="lenses" value=0>
-                </label>
-            </div>
-            <br>
-            <div class="box1-2">
-                <p class="p1-3">原生髮色</p>
-                <p class="p1-4">非原生髮色</p>
-                <label for="radio">
-                        <input type="radio" class="btn3" name="hair_dying" value=1 required="required">
-                </label>
-                <label for="radio">
-                        <input type="radio" class="btn4" name="hair_dying" value=0>
-                </label>
-            </div>
+            
+            <form id="settingsForm" method="post">
+                <div class="box1-2">
+                    <p class="p1-1">男</p>
+                    <p class="p1-2">女</p>
+                    <label>
+                        <input type="radio" class="btn" name="gender" value="1" required>
+                        <span class="custom-btn"></span>
+                    </label>
+                    <label>
+                        <input type="radio" class="btn" name="gender" value="0">
+                        <span class="custom-btn"></span>
+                    </label>
+                </div>
+                <br>
+                <div class="box1-2">
+                    <p class="p1-3">無變色片</p>
+                    <p class="p1-4">有變色片</p>
+                    <label>
+                        <input type="radio" class="btn" name="lenses" value="1" required>
+                        <span class="custom-btn1"></span>
+                    </label>
+                    <label>
+                        <input type="radio" class="btn" name="lenses" value="0">
+                        <span class="custom-btn1"></span>
+                    </label>
+                </div>
+                <br>
+                <div class="box1-2">
+                    <p class="p1-5">無染髮</p>
+                    <p class="p1-6">有染髮</p>
+                    <label>
+                        <input type="radio" class="btn" name="hair_dying" value="1" required>
+                        <span class="custom-btn2"></span>
+                    </label>
+                    <label>
+                        <input type="radio" class="btn" name="hair_dying" value="0">
+                        <span class="custom-btn2"></span>
+                    </label>
+                </div>
+                <br>
+            </form>
         </div>
 
-        <div class="col-md-5 center-section">
+        <div class="col-md-5 col-sm-12 col-12 center-section">
             <p class="t2">-AI臉部辨識分析-</p>
             <div class="box1-1">
-                <form id="uploadForm" enctype="multipart/form-data" method="post">
-                <input type="file" name="photo" id="photo" accept="image/jpeg,image/png" required style="display: none;">
-                <label for="photo">
-                </label>
-                <label for="photo">
-                    <img class="Image1-2" src="images/test_new2.png" alt="Description of the image" class="img-fluid">
-                </label>
-                <button class="button-upload" type="submit">上傳照片！</button>
+                <form id="uploadForm" enctype="multipart/form-data" method="post" action="/upload-photo">
+                    <input type="file" name="photo" id="photo" accept="image/jpeg,image/png" required style="display: none;">
+                    <label for="photo">
+                        <img class="Image1-2" src="images/test_new2.png" alt="Description of the image" class="img-fluid">
+                    </label>
+                    <button class="button-upload" type="submit">上傳照片！</button>
                 </form>
             </div>
             <p class="t3">上傳圖片獲取您的四季色判定！</p>
         </div>
-        <div class="col-md-1 left-section">
+        <div class="col-md-1 col-sm-1 col-1 left-section">
         </div>
     </div>
     <br><br><br>
 </div>
 
-      <!-- 第二區塊 -->
+     <!-- 第二區塊 -->
      <div class="container-fluid block2">
-        <br><br><br><br><br>
+        <br><br>
         <div class="row">
-            <div class="col-md-2 left-section">
+            <div class="col-sm-1">
                 <img class="Image2-1" src="images/test_new3.png" alt="Description of the image" class="img-fluid">
-                <img class="Image2-2" src="images/test_new4.png" alt="Description of the image" class="img-fluid">
-                <img class="Image2-3" src="images/test_new5.png" alt="Description of the image" class="img-fluid">
-                <img class="Image2-4" src="images/test_new6.png" alt="Description of the image" class="img-fluid">
             </div>
-
-            <div class="col-md-9 center-section">
-                <div class="box2">
+            <div class="col-sm-10">
+                <div class="box2-2">
                     <div class="row">
-                    <p class="text2-1">✨ 你知道嗎？你的專屬色調藏在你的DNA裡！✨</p>
-                    <p class="text2-2">韓國歐巴歐膩都在瘋玩的「四季色分析」，現在換你來測測看！<br>不必飛韓國，只要上傳你在自然光源下的清晰照片，系統將會根據你的膚色、瞳色、原生髮色、臉頰和唇色，分析你的命定色系</p>
-                    <p class="text2-3">為什麼要測四季色？</p>
-                    <p class="text2-4">因為每個人天生膚色和血液中的黑色素、血紅素、胡蘿蔔素都不一樣，所以適合的顏色也不同！<br>找到對的顏色，讓你氣色紅潤、膚色均勻，整個人都亮起來！<br>四季色型態分為「春季型」、「夏季型」、「秋季型」、「冬季型」四種，幫你找出最適合你的明度、彩度、色相和清濁度。<br>想知道自己是哪一型嗎？快來試試看，和歐巴歐膩一起變身時尚達人吧！</p>
-                    </div>
+                        <div class="col-sm-1">
+                            <img class="Image2-3" src="images/test_new5.png" alt="Description of the image" class="img-fluid">
+
+                        </div>
+                        <div class="col-sm-10">
+                         <p class="p2-1">✨ 你知道嗎？你的專屬色調藏在你的DNA裡！✨<br><br>韓國歐巴歐膩都在瘋玩的「四季色分析」，現在換你來測測看！<br>不必飛韓國，只要上傳你在自然光源下的清晰照片，系統將會根據你的膚色、瞳色、原生髮色、臉頰和唇色，分析你的命定色系<br><br>
+                            為什麼要測四季色？<br><br>因為每個人天生膚色和血液中的黑色素、血紅素、胡蘿蔔素都不一樣，所以適合的顏色也不同！<br>找到對的顏色，讓你氣色紅潤、膚色均勻，整個人都亮起來！<br>四季色型態分為「春季型」、「夏季型」、「秋季型」、「冬季型」四種，幫你找出最適合你的明度、彩度、色相和清濁度。<br>想知道自己是哪一型嗎？快來試試看，和歐巴歐膩一起變身時尚達人吧！</p>
+                        </div>
+                        <div class="col-sm-1">
+                            <img class="Image2-4" src="images/test_new6.png" alt="Description of the image" class="img-fluid">
+                        </div>
+                    </div>        
                 </div>
             </div>
+            <div class="col-sm-1">
+                <img class="Image2-2" src="images/test_new4.png" alt="Description of the image" class="img-fluid">
+            </div>
+           </div>
+           <br><br>
+        </div>
 
-
-            <div class="col-md-1 right-section">
+    <!-- 第三區塊 -->
+    <div class="container-fluid block5">
+        <div class="row">
+            <div class="col-2">
+            </div>
+            <div class="col-8">
+                <p class="p-5">四季更褶<br>Quatrefoil</p>
+            </div>
+            <div class="col-2">
             </div>
         </div>
-      </div>
+    </div>
