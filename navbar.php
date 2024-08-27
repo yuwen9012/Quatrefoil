0f82cccc13afa6a97c8874539e4a69e910d97f25
helen1020912 <74821347+helen1020912@users.noreply.github.com>
2024-08-27 17:01:43 +0800

    Add files via upload

--- navbar.php
+++ navbar.php
@@ -2,7 +2,17 @@
 <html>
 <head>
 
-<link rel='stylesheet' type='text/css' href='css/Navbar.css'>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel='stylesheet' type='text/css' href='css/navbar.css'>
+
+<script>
+    function toggleMenu() {
+        var navCenter = document.querySelector('.nav_center');
+        navCenter.classList.toggle('active');
+    }
+</script>
+
+
 </head>
 
 <nav>
@@ -14,10 +24,14 @@
       <a href="#"><span>四季商城</span></a>
     </div>
     <div class='nav_right'>
-      <a href="#"><img src="images/icon/shirt-black.png" width="24px" height="24px"/></a>
-      <a href="account_setting.php"><img src="images/icon/user.png" width="23px" height="23px"/></a>
-      <div id="ham_menu"></div>
+    <a href="account_setting.php"><img src="images/icon/user.png" width="23px" height="23px"/></a>
+    <!-- 漢堡選單按鈕 -->
+    <div id="ham_menu" onclick="toggleMenu()">
+        <div></div>
+        <div></div>
+        <div></div>
     </div>
+</div>
   </nav>
 
 </html>
