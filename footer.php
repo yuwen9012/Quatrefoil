402a4480562c1781324cffcac60a94b9cda05722
RyanLin <x56572000@gmail.com>
2024-08-28 11:10:08 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,19 @@
+<link rel="stylesheet" href="css/home.css">
+    
+    
+    
+    <!-- 底部區塊 -->
+    <div class="container-fluid block5" style="background-color: #f9f9f9; height: flex;">
+        <div class="row">
+            <div class="col-4">
+            </div>
+
+            <div class="col-4">
+                <p class="p-5">四季更褶<br>Quatrefoil</p>
+            </div>
+
+
+            <div class="col-4">
+            </div>
+        </div>
+    </div>
