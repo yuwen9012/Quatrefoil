e2c9571afd69b13ef8060d5223bc7adbf5aa9105
yuwen9012 <156352342+yuwen9012@users.noreply.github.com>
2024-08-31 16:13:02 +0800

    Update submit.php

--- php/submit.php
+++ php/submit.php
@@ -9,21 +9,34 @@ if (!$select_db) {
     $response['msg'] = "發生錯誤，請稍後再試(db)";
 } else {
     // 获取表单数据
-    $spring = $_POST['spring'];
-    $summer = $_POST['summer'];
-    $autumn = $_POST['autumn'];
-    $winter = $_POST['winter'];
+    $winter_1 = $_POST['winter_1'];
+    $summer_1 = $_POST['summer_1'];
+    $autumn_1 = $_POST['autumn_1'];
+    $spring_1 = $_POST['spring_1'];
+    $winter_2 = $_POST['winter_2'];
+    $summer_2 = $_POST['summer_2'];
+    $autumn_2 = $_POST['autumn_2'];
+    $spring_2 = $_POST['spring_2'];
+    $winter_3 = $_POST['winter_3'];
+    $summer_3 = $_POST['summer_3'];
+    $autumn_3 = $_POST['autumn_3'];
+    $spring_3 = $_POST['spring_3'];
+    $winter_4 = $_POST['winter_4'];
+    $summer_4 = $_POST['summer_4'];
+    $autumn_4 = $_POST['autumn_4'];
+    $spring_4 = $_POST['spring_4'];
+
 
     // 插入数据到数据库表中
-    $sql = "INSERT INTO test_sorting (spring, summer, autumn, winter) VALUES (?, ?, ?, ?)";
+    $sql = "INSERT INTO test_sorting (winter_1, summer_1, autumn_1, spring_1, winter_2, summer_2, autumn_2, spring_2, winter_3, summer_3, autumn_3, spring_3, winter_4, summer_4, autumn_4, spring_4) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     $stmt = $db_link->prepare($sql);
     
     // 绑定参数
-    $stmt->bind_param("iiii", $spring, $summer, $autumn, $winter);
+    $stmt->bind_param("iiiiiiiiiiiiiiii", $winter_1, $summer_1, $autumn_1, $spring_1, $winter_2, $summer_2, $autumn_2, $spring_2, $winter_3, $summer_3, $autumn_3, $spring_3, $winter_4, $summer_4, $autumn_4, $spring_4);
 
     // 执行查询
     if ($stmt->execute()) {
-         header("Location: test_new.php");
+         header("Location: experiment.php");
     } else {
         echo "插入数据时出错: " . $stmt->error;
     }
