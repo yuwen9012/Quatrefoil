9a1d9cdd2d5b16fbebcd3cfbffe5ce5cd1f1f22e
helen1020912 <74821347+helen1020912@users.noreply.github.com>
2024-08-31 19:20:48 +0800

    Add files via upload

---
+++ database/season_test.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2024-08-31 19:20:04
+-- 伺服器版本： 10.4.32-MariaDB
+-- PHP 版本： 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `quatrefoil`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `season_test`
+--
+
+CREATE TABLE `season_test` (
+  `id` int(11) NOT NULL,
+  `image_name` varchar(50) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `hair_dying` varchar(10) NOT NULL,
+  `lenses` varchar(10) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `timestamp` timestamp(6) NOT NULL DEFAULT current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `season_test`
+--
+ALTER TABLE `season_test`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `season_test`
+--
+ALTER TABLE `season_test`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
