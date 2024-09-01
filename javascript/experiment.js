6ee08f99810d2f6798e04f71ed2600b4d323d014
yuwen9012 <156352342+yuwen9012@users.noreply.github.com>
2024-09-02 02:37:47 +0800

    表單防呆

--- javascript/experiment.js
+++ javascript/experiment.js
@@ -1,3 +1,55 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('orderForm').addEventListener('submit', function(event) {
+        const sections = ['green-section', 'red-section', 'blue-section', 'yellow-section'];
+        let isValid = true; // 追踪验证状态
+
+        // 遍历每个 section 进行验证
+        for (let section of sections) {
+            const inputs = document.querySelectorAll(`#${section} input[type="number"]`);
+            const values = [];
+
+            // 检查是否有未填的输入框
+            for (let input of inputs) {
+                if (input.value === "") {
+                    alert(`请确保 ${section} 内的所有输入框都已填写。`);
+                    isValid = false;
+                    event.preventDefault(); // 阻止表单提交
+                    return; // 跳出循环
+                }
+                values.push(Number(input.value));
+            }
+            console.log('Form isValid:', isValid);
+
+
+            // 检查是否有重复的值
+            const uniqueValues = [...new Set(values)];
+            if (uniqueValues.length !== values.length) {
+                alert(`${section} 内的排序1-4不能重复。`);
+                isValid = false;
+                event.preventDefault(); // 阻止表单提交
+                return; // 跳出循环
+            }
+
+            // 检查是否所有值都在1到4之间
+            for (let value of values) {
+                if (value < 1 || value > 4) {
+                    alert(`${section} 内的排序的值必须在1到4之间。`);
+                    isValid = false;
+                    event.preventDefault(); // 阻止表单提交
+                    return; // 跳出循环
+                }
+            }
+        }
+
+        // 如果有任何验证失败，阻止表单提交
+        if (!isValid) {
+            event.preventDefault(); // 防止表单提交
+        }
+    });
+});
+
+
+
 function get_experiment_image(callback){
 	$.ajax({
 		url: "../php/ajax_load_face_image.php",
@@ -40,3 +92,4 @@ $(document).ready( function () {
     //使用者反饋
 
 });
+
